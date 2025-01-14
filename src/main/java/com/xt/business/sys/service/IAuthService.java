5f220d5b5c96839014a096e4923edbca8ee338dd
Hongxuan Chai <hongxuanchai0@gmail.com>
2025-01-14 16:54:46 +0800

    feat:提交

--- src/main/java/com/xt/business/sys/service/IAuthService.java
+++
@@ -1,8 +0,0 @@
-package com.xt.business.sys.service;
-
-import com.xt.business.sys.domain.dto.AuthDTO;
-import com.xt.business.sys.domain.dto.LoginDTO;
-
-public interface IAuthService {
-    String login(LoginDTO loginDTO);
-}
